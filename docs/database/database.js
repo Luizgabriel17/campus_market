e2ffc5874579965654a2b8525a02740aa1850a29
Luiz Gabriel <gabrielsoua03062006@gmail.com>
2026-04-21 09:16:37 -0300

    database corrigido

--- docs/database/database.js
+++ docs/database/database.js
@@ -1,9 +1,9 @@
-const mysql = require("mysql2");
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD, // mantém esse nome
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
   ssl: {
